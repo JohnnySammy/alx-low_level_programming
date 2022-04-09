362efdcb243a7384f56bc5a1f1d02fe908a45831
JohnnySammy <tobiboyi2020@gmail.com>
2022-04-09 05:22:16 -0700

    Compilation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
-*main -function begins
+*main-function begins
+*
+*Return -function ends
 */
-int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return(0);
 }
