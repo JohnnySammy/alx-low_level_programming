34276465b0834075e76f3e31435d12681ff55281
JohnnySammy <tobiboyi2020@gmail.com>
2022-04-09 17:04:50 -0700

    Prints quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main - Prints last part of quote as standard error
+*main - prints quote 
 *
 *Return: 1 if success
 */
 int main(void)
 {
-write(1, "and that piece of art is useful\"-Dora Korpar\, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
