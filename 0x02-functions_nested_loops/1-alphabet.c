47ca83c30e34d933e91db2ed7b57b17e90ebfc17
JohnnySammy <tobiboyi2020@gmail.com>
2022-04-12 16:13:37 -0700

    Print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,34 +1,15 @@
-#include "main.h”                                                                                                                                                                                    
-
-/**                                                                                                                                                                                                  
-
-*main - Entry point                                                                                                                                                                                  
-
-*                                                                                                                                                                                                    
-
-*print alphabet -Entry point                                                                                                                                                                         
-
-*Return: 0 on success                                                                                                                                                                                
-
-*/                                                                                                                                                                                                   
-
-voidprint_alphabet(void)                                                                                                                                                                             
-
-{                                                                                                                                                                                                    
-
-char c = 'a';                                                                                                                                                                                        
-
-while (c <= 'z')                                                                                                                                                                                     
-
-{                                                                                                                                                                                                    
-
-_putchar(c);                                                                                                                                                                                         
-
-c++;                                                                                                                                                                                                 
-
-}                                                                                                                                                                                                    
-
-_putchar('\n');                                                                                                                                                                                      
-
-}                               
-
+#include 'main.h'
+/**
+*print alphabet -Entry point
+*Return: 0 on success
+*/
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
