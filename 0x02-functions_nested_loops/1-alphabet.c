c11461efa2a907795e47e78538b5f141a8dedb69
JohnnySammy <tobiboyi2020@gmail.com>
2022-04-12 15:49:57 -0700

    Printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,34 @@
+#include "main.h”                                                                                                                                                                                    
+
+/**                                                                                                                                                                                                  
+
+*main - Entry point                                                                                                                                                                                  
+
+*                                                                                                                                                                                                    
+
+*print alphabet -Entry point                                                                                                                                                                         
+
+*Return: 0 on success                                                                                                                                                                                
+
+*/                                                                                                                                                                                                   
+
+voidprint_alphabet(void)                                                                                                                                                                             
+
+{                                                                                                                                                                                                    
+
+char c = 'a';                                                                                                                                                                                        
+
+while (c <= 'z')                                                                                                                                                                                     
+
+{                                                                                                                                                                                                    
+
+_putchar(c);                                                                                                                                                                                         
+
+c++;                                                                                                                                                                                                 
+
+}                                                                                                                                                                                                    
+
+_putchar('\n');                                                                                                                                                                                      
+
+}                               
+
