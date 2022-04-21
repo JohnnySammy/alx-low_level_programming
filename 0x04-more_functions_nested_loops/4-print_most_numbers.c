c371e97ebe0cc9728d0d7992e13f386df186fe70
JohnnySammy <tobiboyi2020@gmail.com>
2022-04-21 07:43:15 -0700

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_most_numbers - Entry point
+*
+* Return: void
+*/
+void print_most_numbers(void)
+{
+int k;
+for (k = 48; k < 58; k++)
+{
+if (k == 50)
+continue;
+else if (k == 52)
+continue;
+_putchar(k);
+}
+_putchar('\n');
+}
