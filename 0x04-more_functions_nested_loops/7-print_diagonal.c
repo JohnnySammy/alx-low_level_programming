78c54965e0ff45d16cd1cb764d63cd6ee257dfd9
JohnnySammy <tobiboyi2020@gmail.com>
2022-04-21 08:09:33 -0700

    draws a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*print_diagonal - Entry point
+*@n: the int to print
+*
+*
+*Return: void
+*/
+void print_diagonal(int n)
+{
+if (n > 0)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+int b;
+for (b = 0; b < i; b++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
