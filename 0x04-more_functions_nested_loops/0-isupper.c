613b41883c80c4f2d2f6a63541b31428fec3e003
JohnnySammy <tobiboyi2020@gmail.com>
2022-04-21 07:31:39 -0700

    Afunction that checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - Entry point
+* @c: the int to print
+*
+*Return: 1 0r 0
+*/
+int _issuper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+else 
+return (0);
+}
