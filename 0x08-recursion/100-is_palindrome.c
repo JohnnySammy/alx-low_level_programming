d6cdf2bd17df10af1d3ca800d47b297c45dc6003
JohnnySammy <tobiboyi2020@gmail.com>
2022-04-30 12:51:44 -0700

    returns the length of a string

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* _strlen_recursion - returns the length of a string.
+* @s: the string to count
+* Return: length of the string
+*/
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+s++;
+return (1 + _strlen_recursion(s));
+}
+return (0);
+}
+/**
+* is_palindrome - checks if the string is a palindrome
+* @s: the string to check
+* Return: 1 if string is a palindrome, 0 if it is not.
+*/
+int is_palindrome(char *s)
+{
+int len = _strlen_recursion(s);
+int count = 0;
+return (checker(s, len -1, count));
+}
