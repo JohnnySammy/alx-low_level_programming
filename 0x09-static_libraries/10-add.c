bc72eee2f8d755d52af5f409dcd5a144d549ce91
JohnnySammy <tobiboyi2020@gmail.com>
2022-05-07 16:21:05 -0700

    static libraries

--- 0x09-static_libraries/10-add.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
-*add 2 numbers and prints the answer
-*Return: 0 on sucess
-*/
-int add(int i, int j)
-{
-return (i + j);
-}
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
-* _putchar - writes the character c to stdout
+* _putchar - Writes the character c to stdout
 * @c: The character to print
-*Return: successs 1
+*Return: on success 1
 *on error - 1 is returned, and errno is set appropriately
 */
 int _putchar(char c)
 {
-return(write(1, &c, 1));
+return (write(1, & c, 1));
 }
